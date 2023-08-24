88334495597e8e854ac96ca1dd36306f817cc12a
Marcelo de Abreu <marcelodeabreu@Marcelos-MacBook-Pro.local>
2023-08-24 17:51:55 -0300

    3commit

--- csgoSkinMarket/Views/ProfileView.swift
+++ csgoSkinMarket/Views/ProfileView.swift
@@ -12,6 +12,27 @@ struct ProfileView: View {
         ZStack {
             Image("csgoSkinMarketBackground1")
                 .opacity(0.5)
+            
+            VStack {
+                // Profile page
+                
+                Image(systemName: "person")
+                    .font(.system(size: 30))
+                    .frame(width: 60, height: 60)
+                    .foregroundColor(.black)
+                    .overlay(RoundedRectangle(cornerRadius: 30)
+                        .stroke(Color.black, lineWidth: 3))
+                
+                HStack {
+                    
+                    Text("(FREE) Username")
+                        .font(.system(size: 15))
+                    
+                    Text("Balance: $150.00")
+                        .font(.system(size: 15))
+                }
+                .padding()
+            }
         }
     }
 }
